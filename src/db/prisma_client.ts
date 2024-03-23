028233f16a17f469e3cc56c199be16b353ba83da
julian-yamori <julian.lampyridae@gmail.com>
2024-03-23 19:55:23 +0000

    PrismaClient の import 先を自前の定義に変更

--- src/db/prisma_client.ts
+++ src/db/prisma_client.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "j-gitadora-psup/node_modules/@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prismaClient = new PrismaClient();
 export default prismaClient;
